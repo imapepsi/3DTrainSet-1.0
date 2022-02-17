6c78cf33ca384d8b6900e642daa4ebae8b9567c5
Mia <miaseppi@gmail.com>
2022-02-17 16:49:50 -0700

    Finished Updating lower car function

--- src/CarTypeBObject.py
+++ src/CarTypeBObject.py
@@ -7,16 +7,21 @@ class CarTypeB(Train):
     def _createLowerCar(self):
         lowerWidth = self._width * (7 / 8)
         lowerHeight = self._height * (7 / 8)
+        lowerBoxBevel = 0.3
+        lowerBoxPositionY = -(self._width * 1.5) + 0.5
+
+        cutHoleWidth = lowerWidth + 1.0
+        cutHoleHeight = lowerHeight - 2
         cutHoleDepth = self._depth - 2
 
         lowerBox = mc.polyCube(w=lowerWidth, h=lowerHeight, d=self._depth, name="lowerBodyBase#")
-        mc.polyBevel(lowerBox[0], offset=0.3)
+        mc.polyBevel(lowerBox[0], offset=lowerBoxBevel)
 
-        cutHole = mc.polyCube(w=lowerWidth + 1.0, h=lowerHeight - 2, d=cutHoleDepth, name="cutHole#")
+        cutHole = mc.polyCube(w=cutHoleWidth, h=cutHoleHeight, d=cutHoleDepth, name="cutHole#")
         lowerBox = mc.polyBoolOp(lowerBox[0], cutHole[0], op=2, n="lowerBodyBaseA#")
 
         mc.select(lowerBox[0], r=True)
-        mc.move(-(self._width * 1.5) + 0.5, y=True, absolute=True)
+        mc.move(lowerBoxPositionY, y=True, absolute=True)
 
         return lowerBox
 
--- src/EngineObject.py
+++ src/EngineObject.py
@@ -16,19 +16,27 @@ class Engine(TrainObject.Train):
         boxPanelWidth = lowerWidth-1
         boxPanelHeight = lowerHeight-1
 
-        boxPanel = mc.polyCube(w=boxPanelWidth, h=boxPanelHeight, d=1, name="lowerBoxPanel#")
-        mc.move(-self._depth, z=True, absolute=True)
+        outerTubeRadius = lowerWidth / 2
+        outerTubeHeight = self._depth
+        innerTubeRadius = lowerWidth / 4
+        innerTubeHeight = self._depth + 2
+        innerTubeSubDiv = 8  # Octagon Shape
+        ringRadius = outerTubeRadius + 0.2
+        tubePositionY = -(lowerHeight * 3)
 
         lowerBox = mc.polyCube(w=lowerWidth, h=lowerHeight, d=self._depth, name="lowerBodyBase#")
         mc.polyBevel(lowerBox[0], offset=lowerBoxBevel)
 
+        boxPanel = mc.polyCube(w=boxPanelWidth, h=boxPanelHeight, d=1, name="lowerBoxPanel#")
+        mc.move(-self._depth, z=True, absolute=True)
+
+        # Combine panel and lower box
         lowerBox = mc.polyBoolOp(lowerBox[0], boxPanel[0], op=2, n="lowerBox#")
         mc.move(lowerBoxPositionY, y=True, absolute=True)
         mc.delete(lowerBox[0], constructionHistory=True)
 
-        octagon = 8
-        innerTube = mc.polyCylinder(r=lowerWidth / 4, h=self._depth+2, subdivisionsAxis=octagon, name="innerTube#")
-        outerTube = mc.polyCylinder(r=lowerWidth / 2, h=self._depth, name="outerTube#")
+        innerTube = mc.polyCylinder(r=innerTubeRadius, h=innerTubeHeight, subdivisionsAxis=innerTubeSubDiv, name="innerTube#")
+        outerTube = mc.polyCylinder(r=outerTubeRadius, h=outerTubeHeight, name="outerTube#")
         tube = mc.polyBoolOp(outerTube[0], innerTube[0], op=1, n="tube#")
         mc.delete(tube[0], constructionHistory=True)
 
@@ -36,13 +44,14 @@ class Engine(TrainObject.Train):
         numRings = self._depth/distBetweenRings
         startY = -self._depth/2 + distBetweenRings
         for y in range(int(startY), int(numRings), distBetweenRings):
-            ring = mc.polyTorus(r=(lowerWidth/2) + 0.2, tw=0.5)
+            ring = mc.polyTorus(r=ringRadius, n="ring#")
             mc.move(y, y=True, absolute=True)
             tube = mc.polyBoolOp(tube[0], ring[0], op=1, n="tube#")
             mc.delete(tube[0], constructionHistory=True)
 
-        mc.rotate(90, x=True, absolute=True)
-        mc.move(-(lowerHeight * 3), y=True, absolute=True)
+        mc.select(tube[0], r=True)
+        mc.rotate(90, x=True, absolute=True)  # Vertical to horizontal
+        mc.move(tubePositionY, y=True, absolute=True)
         mc.delete(tube[0], constructionHistory=True)
 
         lowerCompartment = mc.polyBoolOp(lowerBox[0], tube[0], op=1, n="lowerCompartment#")
